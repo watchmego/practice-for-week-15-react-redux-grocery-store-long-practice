c68a97c4cc9f291bfcad1915f51efc27d18ab572
Reuben White <white.reuben@gmail.com>
2022-11-24 10:18:08 +1300

    setup completed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import './index.css';
 import App from './App';
 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
 function Root() {
   return (
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </Provider>
+      
   );
 }
 
@@ -18,3 +29,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+import { combineReducers, applyMiddleware, compose, createStore} from "redux";
+
+const rootReducer = combineReducers({
+
+});
+
+let enhancer;
+
+if (process.env.NODE_ENV !== "production") {
+    const logger = require("redux-logger").default;
+    const composeEnhancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    enhancer = composeEnhancers(applyMiddleware(logger));
+}
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+}
+
+export default configureStore;
